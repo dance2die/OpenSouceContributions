91faae61856dcc63472056589e9998aa6398a72e
Sung Kim <sungmkim80@gmail.com>
2018-05-17 17:51:18 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# List of Contributions
+
+This is a github page for tracking list of FOSS (Free/Open Source Software) contributions made.
+
+Link to the page: https://dance2die.github.io/OpenSourceContributions
---
+++ src/components/Bio.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.png'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Sung Kim`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Tracked by <strong>Sung Kim</strong> who lives and works in New York
+          City building useful(and many useless)things.{' '}
+          <a href="https://twitter.com/slightedgecoder">
+            You should follow him on Twitter
+          </a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio

84c3cfe3c47977a21910665c2d09df285767803e
Sung Kim <sungmkim80@gmail.com>
2018-05-19 23:05:41 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/OpenSouceContributions/app-0ca18bafdd124dd035ec.js","2897f874d35611a1513aaa48f6ee85ad"],["/OpenSouceContributions/app-2d2239659a4b604191c8.js","78b430db62d5969ac804f1dc4ac9ba80"],["/OpenSouceContributions/commons-b473c4ddef8d57bfbb89.js","fbe03f23e7509b1cadaa90be95dc4c74"],["/OpenSouceContributions/index.html","3a89decc852558fb5c6d85cbfdec61b8"],["/OpenSouceContributions/offline-plugin-app-shell-fallback/index.html","b8383b587fedc1a0c30adf047b49d6c1"]];
+var precacheConfig = [["/OpenSouceContributions/app-0ca18bafdd124dd035ec.js","2897f874d35611a1513aaa48f6ee85ad"],["/OpenSouceContributions/app-2d2239659a4b604191c8.js","78b430db62d5969ac804f1dc4ac9ba80"],["/OpenSouceContributions/commons-b473c4ddef8d57bfbb89.js","fbe03f23e7509b1cadaa90be95dc4c74"],["/OpenSouceContributions/index.html","18c1bc95c0ab20f4dfc20c28d92c7e11"],["/OpenSouceContributions/offline-plugin-app-shell-fallback/index.html","4ca59f8b5ea8a6c5c0125e9bccd3cea9"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
